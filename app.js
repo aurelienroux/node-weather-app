15959c865d445da323ae1089d1b08205001f7cd0
Aurélien Roux <aureroux13@gmail.com>
2020-11-15 18:09:18 -0500

    request to weatherstack

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('node')
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=a8b3a7547774dbb07001c99a4e902fd2&query=munich'
+
+request(url, (err, resp) => {
+  if (err) {
+    console.log(err) // eslint-disable-line
+    return
+  }
+
+  const data = JSON.parse(resp.body)
+  console.log(data.current) // eslint-disable-line
+})
