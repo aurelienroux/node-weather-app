d51f80939748ef7a9869729a8555e4a193775d99
Aurélien Roux <aureroux13@gmail.com>
2020-11-16 15:24:55 -0500

    weatherstack forecast

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const request = require('request')
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=munich`
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=berlin`
 
-request(url, (err, resp) => {
-  if (err) {
-    console.log(err) // eslint-disable-line
-    return
-  }
-
-  const data = JSON.parse(resp.body)
-  console.log(data.location) // eslint-disable-line
+request({ url, json: true }, (err, resp) => {
+  console.log( // eslint-disable-line
+    `
+    It is currently ${resp.body.current.temperature} degrees in ${resp.body.location.name}.
+    It feels like ${resp.body.current.feelslike} degrees.
+    Weather conditions are ${resp.body.current.weather_descriptions[0]}
+    `
+  )
 })
